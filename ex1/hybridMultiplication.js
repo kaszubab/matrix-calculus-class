5512302e9d37616b865979e664c146bb96cdd7f4
Bartosz Kaszuba <bartosz.kaszuba@swmansion.com>
2022-03-12 18:00:50 +0100

    Minor fix to hybrid algorithm

--- ex1/hybridMultiplication.js
+++ ex1/hybridMultiplication.js
@@ -1,10 +1,17 @@
 const { strassenAlgorithm, strassenAlgorithmWithOperationCounting } = require('./strassenMultiplication')
 const { basicMultiplication, basicMultiplicationWithOperationCounting } = require('./basicMultiplication')
 
-const hybridMultiplication = (matrixA, matrixB, thresholdSize) =>
-  matrixA.length > thresholdSize
-    ? strassenAlgorithm(matrixA, matrixB, hybridMultiplication)
-    : basicMultiplication(matrixA, matrixB)
+const hybridMultiplication = (matrixA, matrixB, thresholdSize) => {
+  const recurrenceCounting = (matrixA, matrixB) => {
+    if (matrixA.length > thresholdSize) {
+      return strassenAlgorithm(matrixA, matrixB, recurrenceCounting)
+    } else {
+      return basicMultiplication(matrixA, matrixB)
+    }
+
+  }
+  return recurrenceCounting(matrixA, matrixB)
+}
 
 const hybridMultiplicationWithOperationCounting = (matrixA, matrixB, thresholdSize) => {
   const recurrenceCounting = (matrixA, matrixB) => {
@@ -28,13 +35,13 @@ const generateMatrix = (size) => {
   return matrix
 }
 
-const test1 = generateMatrix(512)
-const test2 = generateMatrix(512)
+const test1 = generateMatrix(256)
+const test2 = generateMatrix(256)
 
 // console.log(test1)
 // console.log(test2)
 
 // console.log(hybridMultiplication(test1, test2, 8))
 // console.log(strassenAlgorithmWithOperationCounting(test1, test2)[1])
-console.log(hybridMultiplicationWithOperationCounting(test1, test2, 32)[1])
-console.log(basicMultiplicationWithOperationCounting(test1, test2)[1])
+console.log(hybridMultiplication(test1, test2, 32))
+// console.log(basicMultiplicationWithOperationCounting(test1, test2)[1])
