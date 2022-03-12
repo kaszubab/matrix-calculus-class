f156cdeaaa3c7a15bb000a4866d956af30b6a1c9
Bartosz Kaszuba <bartosz.kaszuba@swmansion.com>
2022-03-12 14:53:00 +0100

    Fixed hybrid algorithm

--- ex1/basicMultiplication.js
+++ ex1/basicMultiplication.js
@@ -20,15 +20,16 @@ const basicMultiplicationWithOperationCounting = (matrixA, matrixB) => {
     for (let i = 0; i < matrixA.length; i++) {
         sumMatrix.push([]);
         for (let j = 0; j < matrixB.length; j++) {
-            let sum = 0;
-            for (let k = 0; k < matrixB.length; k++) {
+            let sum = matrixA[i][0] * matrixB[0][j];
+            fpOperations += 1;
+            for (let k = 1; k < matrixB.length; k++) {
                 sum += matrixA[i][k] * matrixB[k][j];
-                fpOperations += 1
+                fpOperations += 2;
             }
             sumMatrix[i].push(sum);
         }
     }
-    return  [sumMatrix, fpOperations];
+    return [sumMatrix, fpOperations];
 };
 
 
@@ -37,4 +38,4 @@ module.exports = {
     basicMultiplication,
     basicMultiplicationWithOperationCounting
 }
-  
+
--- ex1/hybridMultiplication.js
+++ ex1/hybridMultiplication.js
@@ -6,26 +6,35 @@ const hybridMultiplication = (matrixA, matrixB, thresholdSize) =>
     ? strassenAlgorithm(matrixA, matrixB, hybridMultiplication)
     : basicMultiplication(matrixA, matrixB)
 
-const hybridMultiplicationWithOperationCounting = (matrixA, matrixB, thresholdSize) =>
-  matrixA.length > thresholdSize
-    ? strassenAlgorithmWithOperationCounting(matrixA, matrixB, hybridMultiplicationWithOperationCounting)
-    : basicMultiplicationWithOperationCounting(matrixA, matrixB)
+const hybridMultiplicationWithOperationCounting = (matrixA, matrixB, thresholdSize) => {
+  const recurrenceCounting = (matrixA, matrixB) => {
+    if (matrixA.length > thresholdSize) {
+      return strassenAlgorithmWithOperationCounting(matrixA, matrixB, recurrenceCounting)
+    } else {
+      return basicMultiplicationWithOperationCounting(matrixA, matrixB)
+    }
+  }
+  return recurrenceCounting(matrixA, matrixB)
+}
 
 const generateMatrix = (size) => {
   const matrix = []
   for (let i = 0; i < size; i++) {
     matrix.push([])
     for (let j = 0; j < size; j++) {
-      matrix[i].push(1)
+      matrix[i].push(Math.random())
     }
   }
   return matrix
 }
 
-const test1 = generateMatrix(32)
-const test2 = generateMatrix(32)
+const test1 = generateMatrix(512)
+const test2 = generateMatrix(512)
 
-console.log(test1)
-console.log(test2)
+// console.log(test1)
+// console.log(test2)
 
-console.log(hybridMultiplication(test1, test2, 0))
+// console.log(hybridMultiplication(test1, test2, 8))
+// console.log(strassenAlgorithmWithOperationCounting(test1, test2)[1])
+console.log(hybridMultiplicationWithOperationCounting(test1, test2, 32)[1])
+console.log(basicMultiplicationWithOperationCounting(test1, test2)[1])
